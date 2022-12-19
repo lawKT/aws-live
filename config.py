317f4de3fe63edc75862836838d59be87c56e903
lawKT <109721747+lawKT@users.noreply.github.com>
2022-12-19 11:10:21 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cw3rliytqjar.us-east-1.rds.amazonaws.com"
+customhost = "employee.cm13buus1fpf.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "lawkuanteng-employee"
 customregion = "us-east-1"
